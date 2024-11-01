94a885bd9edc715635d57aadda46a2764bc1ae75
Chik <chixide12@gmail.com>
2024-11-01 23:26:06 +0000

    feat - added Access Token endpoint in server

---
+++ PersonalFinanceApp.Server/Program.cs
@@ -0,0 +1,36 @@
+using Going.Plaid;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Plaid Config
+builder.Services.AddPlaid(builder.Configuration.GetSection("Plaid"));
+builder.Services.AddSingleton<PlaidClient>();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
